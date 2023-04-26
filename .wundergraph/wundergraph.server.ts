f20942e4338d72619dba02aa70478ed3356b4e8c
Suvij Surya <suvijsurya76@gmail.com>
2023-04-26 16:33:10 +0530

    Update wundergraph.server.ts

--- .wundergraph/wundergraph.server.ts
+++ .wundergraph/wundergraph.server.ts
@@ -1,12 +1,26 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
-import { configureWunderGraphServer } from '@wundergraph/sdk/server';
-import type { HooksConfig } from './generated/wundergraph.hooks';
-import type { InternalClient } from './generated/wundergraph.internal.client';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import {
+  configureWunderGraphServer,
+  EnvironmentVariable,
+} from "@wundergraph/sdk/server";
+import type { HooksConfig } from "./generated/wundergraph.hooks";
+import type { InternalClient } from "./generated/wundergraph.internal.client";
 
 export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
-	hooks: {
-		queries: {},
-		mutations: {},
-	},
-	graphqlServers: [],
+  hooks: {
+    queries: {
+      Dragons: {
+        mutatingPostResolve: async (hook) => {
+          if (hook.response.errors) return hook.response;
+          hook.response.data?.spacex_dragons?.push({
+            name: process.env.foo,
+            active: true,
+          });
+          return hook.response;
+        },
+      },
+    },
+    mutations: {},
+  },
+  graphqlServers: [],
 }));
